b21332629bf227717794d0d5130b8c82932ee9ac
Chaim Shteinberg <a0533197133@gmail.com>
2025-06-16 14:30:13 +0300

    random sensor

---
+++ InvestigationGame/Logic/GetRandomSensor.cs
@@ -0,0 +1,25 @@
+﻿using InvestigationGame.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvestigationGame.Logic
+{
+    internal class GetRandomSensor
+    {
+        public static ISensor GetSensor()
+        {
+            ISensor sensor;
+
+            AudioSensor audioSensor = new AudioSensor("AudioSensor");
+            ThermalSensor thermalSensor = new ThermalSensor("ThermalSensor");
+
+            Random random = new Random();
+            sensor = random.Next(2) == 0 ? audioSensor : thermalSensor;
+
+            return sensor;
+        }
+    }
+}
