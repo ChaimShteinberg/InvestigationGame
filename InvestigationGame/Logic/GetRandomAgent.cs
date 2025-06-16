550f7f8f34d5b54d6ad4b276fb57b329cea7a073
Chaim Shteinberg <a0533197133@gmail.com>
2025-06-16 14:59:05 +0300

    random Agent

---
+++ InvestigationGame/Logic/GetRandomAgent.cs
@@ -0,0 +1,29 @@
+﻿using InvestigationGame.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvestigationGame.Logic
+{
+    internal class GetRandomAgent
+    {
+        public static IIranianAgent GetSensor()
+        {
+            IIranianAgent sensor;
+            List<ISensor> weaknesses = new List<ISensor>();
+            List<ISensor> injuries = new List<ISensor>();
+
+
+            FootSolider footSolider = new FootSolider("AudioSensor", weaknesses, injuries);
+
+            //Random random = new Random();
+            //sensor = random.Next() == 0 ? footSolider;
+
+            sensor = footSolider; // זמני
+
+            return sensor;
+        }
+    }
+}
--- InvestigationGame/obj/Debug/net8.0/InvestigationGame.AssemblyInfo.cs
+++ InvestigationGame/obj/Debug/net8.0/InvestigationGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InvestigationGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d89f99e9a0e51366e8d1686b1f797e09a4a36c93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b21332629bf227717794d0d5130b8c82932ee9ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("InvestigationGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InvestigationGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
