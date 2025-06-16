0c58c9c1e8beb8356e9b586c20fa2aab4d2f9d4e
Chaim Shteinberg <a0533197133@gmail.com>
2025-06-16 11:27:39 +0300

    sensors models

---
+++ InvestigationGame/models/AudioSensor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvestigationGame.models
+{
+    internal class AudioSensor:ISensor
+    {
+        public string Name { get; set; }
+
+        public AudioSensor(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
